39d81c55eb7c578d9f760734f0addf27d9ca37cb
Rafal <rf.zajac@gmail.com>
2023-09-19 17:12:18 +0200

    Started to work on firebase login

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,5 +1,7 @@
 import { ReactNode, createContext, useState } from "react";
 import { User } from "../types/types";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 // ? TYPES
 interface AuthContextType {
@@ -28,6 +30,23 @@ export const AuthContext = createContext<AuthContextType>(AuthInitContext);
 
 export const AuthContextProvider = ({children}: AuthContextProviderProps) => {
     
+    // Your web app's Firebase configuration
+    const firebaseConfig = {
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_APPID
+    };
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+
+    // Initialize Firebase Authentication and get a reference to the service
+    const auth = getAuth(app);
+    console.log(auth)
+
     const [user, setUser] = useState<User | null>(null)
 
     const login = () => {
